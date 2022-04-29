ea989ceb541ebf38c443384d2bc3f7f7991972a1
george <armstronggeorge10@gmail.com>
2022-04-29 16:32:46 +1000

    make it so

--- backend/index.ts
+++ backend/index.ts
@@ -1,16 +1,27 @@
-import { get, getKnex, logger, post, uuid, wait } from "./lib";
+/* eslint-disable no-underscore-dangle */
+import _ from "lodash";
+import fs from "fs";
+import path from "path";
+import { getKnex, logger, post, uuid, wait } from "./lib";
 
 const API_BASE_URL = "https://api.trongrid.io";
 
-const getAccountFirstTransaction = async (account: string) => {
-  const transactions = await get(
-    `${API_BASE_URL}/v1/accounts/${account}/transactions?limit=1&order_by=block_timestamp%2Casc`
-  );
+// blocks from dec 31 of each year
 
-  const [trx] = transactions.data;
-  console.log({ trx });
-  return trx;
-};
+const _2020 = [2020, 26363366];
+const _2021 = [2021, 36795624];
+
+const years = [_2020, _2021];
+
+// const getAccountFirstTransaction = async (account: string) => {
+//   const transactions = await get(
+//     `${API_BASE_URL}/v1/accounts/${account}/transactions?limit=1&order_by=block_timestamp%2Casc`
+//   );
+
+//   const [trx] = transactions.data;
+
+//   return trx;
+// };
 
 const getAccountInfoAtLedger = async (account: string, block: number) => {
   const blockData = await post(`${API_BASE_URL}/wallet/getblockbynum`, {
@@ -82,22 +93,90 @@ const getNextBalance = async () => {
   await getNextBalance();
 };
 
-const start = async () => {
+export const parseCsv: (csv: string) => string[] = _.flow([
+  // remove headings
+  (data) => _.split(data, "\n"),
+  (data) => _.drop(data, 1),
+  (data) => _.map(data, (data) => _.split(data, ",")),
+]);
+
+const ADDRESS_FILE = "address.csv";
+
+const loadAddresses = async () => {
   const knex = await getKnex();
-  const [, , account = "TC74QG8tbtixG5Raa4fEifywgjrFs45fNz"] = process.argv;
-
-  const [previousRecord] = await knex("balance")
-    .orderBy("date", "desc")
-    .limit(1);
-
-  if (!previousRecord) {
-    const data = await getAccountFirstTransaction(account);
-    console.log({ data });
-    const accountInfo = await getAccountInfoAtLedger(account, data.blockNumber);
-    await insertBalanceRow(accountInfo);
-    console.log({ accountInfo });
+  const csvStr = fs.readFileSync(path.join(__dirname, ADDRESS_FILE), "utf-8");
+  const csvRows = parseCsv(csvStr);
+  for (const chunk of _.chunk(csvRows, 1000)) {
+    await knex("account")
+      .insert(
+        chunk.map(([address, createdAt]) => ({
+          id: uuid(),
+          address,
+          created_at: new Date(createdAt),
+        }))
+      )
+      .onConflict("address")
+      .ignore();
   }
-  getNextBalance();
+};
+
+const start = async () => {
+  console.log("here");
+  const knex = await getKnex();
+  const [, , load = true] = process.argv;
+
+  if (load) {
+    await loadAddresses();
+  }
+
+  for (const [year, block] of years) {
+    let next = true;
+    let page = 0;
+    while (next) {
+      const accounts = await knex("account")
+        .whereRaw("created_at <= make_date(?, ?, ?)", [year, 12, 31])
+        .whereNotIn(
+          "id",
+          knex("balance")
+            .whereRaw("date =  make_date(?, ?, ?)", [year, 12, 31])
+            .select("account_id")
+        )
+        .limit(1000)
+        .offset(page * 1000)
+        .orderBy("address", "desc");
+
+      if (_.isEmpty(accounts)) {
+        next = false;
+        break;
+      } else {
+        // eslint-disable-next-line no-plusplus
+        page++;
+      }
+
+      const [{ date }] = (
+        await knex.raw("select make_date as date from make_date(?, ?, ?)", [
+          year,
+          12,
+          31,
+        ])
+      ).rows;
+
+      for (const account of accounts) {
+        const balance = await getAccountInfoAtLedger(account.address, block);
+
+        await knex("balance")
+          .insert({
+            id: uuid(),
+            account_id: account.id,
+            balance: balance.balance,
+            date,
+            created_at: new Date(),
+          })
+          .onConflict(["account_id", "date"])
+          .ignore();
+      }
+    }
+  }
 };
 
 start();
--- migrations/20220226035910_initial.js
+++ migrations/20220226035910_initial.js
@@ -3,13 +3,18 @@
  * @returns { Promise<void> }
  */
 exports.up = async (knex) => {
+  await knex.schema.createTable('account', (table) => {
+    table.string('id').primary().index();
+    table.string('address').notNullable().unique();
+    table.timestamp('created_at');
+  });
   await knex.schema.createTable('balance', (table) => {
     table.string('id').primary().index();
     table.float('balance').notNullable();
-    table.integer('block').notNullable();
-    table.string('account').notNullable();
+    table.string('account_id').notNullable().references('account.id');
     table.timestamp('date').notNullable();
     table.timestamp('created_at');
+    table.unique(['account_id', 'date']);
   });
 };
 
